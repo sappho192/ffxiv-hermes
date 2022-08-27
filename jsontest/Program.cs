d6588583480771f5a15fe4fa9df68d8d5f0c95f2
Sappho <sappho192@gmail.com>
2022-08-27 13:22:05 +0900

    Add jsontest project

---
+++ jsontest/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace jsontest
+{
+    [JsonObject]
+    public class HermesAddress
+    {
+        public string Name { get; set; }
+        public List<long> Address { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new List<long>
+                {
+                    //0x01EC0F40
+                    0x01FDC4E8,
+                    0x8L,
+                    0x18L,
+                    0x20L,
+                    0x100L,
+                    0x0L
+                };
+
+            var address = new HermesAddress();
+            address.Name = "NPCDialogue";
+            address.Address = map;
+
+            var json = JsonConvert.SerializeObject(address);
+            Console.WriteLine(json);
+
+            //var url = "https://raw.githubusercontent.com/sappho192/ffxiv-hermes/main/latest/address.json";
+            //using (WebClient client = new WebClient())
+            //{
+            //    string s = client.DownloadString(url);
+            //    var jsonread = JsonConvert.DeserializeObject<HermesAddress>(s);
+            //    Console.WriteLine(jsonread.Name);
+            //    Console.WriteLine(string.Join(", ", jsonread.Address));
+            //}
+        }
+    }
+}
